887c0b7ba7939f55d00ebdd1f7a283244fc4d397
rkajitani <rei.kajitani@gmail.com>
2023-04-26 16:46:09 +0900

    modify left-join script to accept options

--- tsv_left_join.py
+++ tsv_left_join.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
 import sys
-
-if len(sys.argv) != 5:
-    print("usage:", sys.argv[0], "left.tsv right.tsv left_col_index right_col_index(0-origin)", file=sys.stderr)
-    exit(0)
-
+import argparse
 import pandas as pd
 
-left_df = pd.read_table(sys.argv[1], sep="\t", header=None, index_col=int(sys.argv[3]), dtype=object)
-right_df = pd.read_table(sys.argv[2], sep="\t", header=None, index_col=int(sys.argv[4]), dtype=object)
+parser = argparse.ArgumentParser(description="Script to left-join dataframe pair.", formatter_class=argparse.MetavarTypeHelpFormatter)
+
+parser.add_argument("left_df_file", type=str, help="left dataframe (TSV file)")
+parser.add_argument("right_df_file", type=str, help="right dataframe (TSV file)")
+parser.add_argument("-l", "--left_i", type=int, default=0, help="index of left dataframe (0-based)")
+parser.add_argument("-r", "--right_i", type=int, default=0, help="index of right dataframe (0-based)")
+parser.add_argument("-n", "--na_str", type=str, default="", help="string of NA value")
+
+args = parser.parse_args()
+
+
+left_df = pd.read_table(args.left_df_file, sep="\t", header=None, index_col=args.left_i, dtype=object)
+right_df = pd.read_table(args.right_df_file, sep="\t", header=None, index_col=args.right_i, dtype=object)
 left_df.index = left_df.index.astype(str)
 right_df.index = right_df.index.astype(str)
 merged_df = pd.merge(left_df, right_df, left_index=True, right_index=True, how="left")
+merged_df.fillna(args.na_str, inplace=True)
 merged_df.to_csv(sys.stdout, sep="\t", header=None)
